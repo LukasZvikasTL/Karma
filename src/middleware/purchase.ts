9b6e593109968e5de0168a9ac5699436af9cd8ba
lukaszvikas <lukas@truelayer.com>
2019-11-22 13:20:40 +0000

    purchases

--- src/middleware/purchase.ts
+++ src/middleware/purchase.ts
@@ -3,7 +3,7 @@ import { User } from "../model/User";
 
 export const getPurchase = async (id: string) => {
   try {
-    return await axios.get(`http://localhost:8080/${id}`);
+    return await axios.get(`http://localhost:8080/purchases/${id}`);
   } catch (err) {
     console.log("err", err);
   }
